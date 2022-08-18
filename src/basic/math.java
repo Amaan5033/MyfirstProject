1fb9def9fd2a599b89b61b1935e347409fbe4804
Amaan5033 <amaan4743@gmail.com>
2022-08-18 17:38:06 +0530

    chnages i made

--- src/basic/math.java
+++ src/basic/math.java
@@ -1,5 +1,17 @@
 package basic;
 
 public class math {
-
+	public void add(int a, int b)
+	{
+		System.out.println("Sum is: ", +a+b);
+	}
+	
+	public void add(int a, int b, int c)
+	{
+		System.out.println("Sum is: ", + a+b+c);
+	}
+	public void multiply(int a, int b)
+	{ 
+		System.out.println("Product is: "+a*b);
+	}
 }
